4c5c5bdf49077727803aaa40bda5c3ba5bb27889
Raquel Vargas V <raquelesthela@yahoo.com>
2019-03-08 15:52:38 -0500

    Create twitter__credentials.py

---
+++ twitter__credentials.py
@@ -0,0 +1,6 @@
+#### You just need to copy and paste your credentials
+
+ACCESS_TOKEN = "******* your access token **********"
+ACCESS_TOKEN_SECRET = " ******* your access token secret ************"
+CONSUMER_KEY = "************ your consumer key **************"
+CONSUMER_SECRET = "********** your consumer secret ************"
